e12c6ae7a5c4aef9b0cc56fb7c0d73a7c4b5bb64
Jack McCoy <67011435+JackRMccoy@users.noreply.github.com>
2020-09-03 20:42:15 -0400

    python basics

---
+++ 00 python basics/00 python basics.py
@@ -0,0 +1,25 @@
+#Hello World
+print("Hello World")
+
+#variable
+n = 3
+print(n)
+
+#function - fibonacci
+def function1(j):
+    return j + 3
+    
+print(function1(7))
+
+#list
+jackList = [3,2,7,4]
+print(jackList)
+
+#2D list
+jack2dList = [[3,2],
+              [7,4]]
+print(jack2dList)
+
+#hashmaps
+dict = {"first_name": "Jack", "last_name": "McCoy" }
+print(dict)
